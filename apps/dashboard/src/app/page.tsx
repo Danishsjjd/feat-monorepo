525cd0d7f7e98bf23e03ada6e1929a26f6987bd9
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-03-20 18:08:04 +0500

    feat: prettier

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-7xl font-bold text-center">Hello world</h1>
+      <h1 className="text-center text-7xl font-bold">Hello world</h1>
     </main>
   )
 }
