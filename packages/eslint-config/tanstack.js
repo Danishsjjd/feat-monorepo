20df6a3608924431f10ab0381f96a9739687416e
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-03-25 17:54:43 +0500

    fix: tanstack eslint config

--- packages/eslint-config/tanstack.js
+++ packages/eslint-config/tanstack.js
@@ -1,12 +1,6 @@
-const { resolve } = require("node:path")
-
-const project = resolve(process.cwd(), "tsconfig.json")
-
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   plugins: ["@tanstack/query"],
-  ignorePatterns: [".*.js", "node_modules/"],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
   rules: {
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@tanstack/query/exhaustive-deps": "warn",
