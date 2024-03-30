2ad14fa3f28ed640b5d10ba6fa9d713af4f8c1bd
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-03-30 14:51:19 +0500

    fmt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,1 @@
-module.exports = require("./packages/tailwind-config/tailwind.config"); // relative path to your repo that uses tailwindcss
+module.exports = require("./packages/tailwind-config/tailwind.config")
